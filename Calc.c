093021050e33817b0a445e133c0c3eed3459afcd
Prawesome <prajithpremg@gmail.com>
2012-08-10 10:16:33 +0300

    second commit

---
+++ Calc.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int a,b,c;
+    printf("Enter the first number to be added:");
+    scanf("%d",&a);
+    printf("Enter the second number to be added:");
+    scanf("%d",&b);
+    c=a + b;
+    printf("%d + %d =%d\n",a,b,c);
+    getchar();
+    return 0;
+}
